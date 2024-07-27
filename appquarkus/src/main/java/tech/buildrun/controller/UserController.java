cf15fda095f842a9412c8f5fc910168aed40f3fa
Carlos Henrique <ichfs9108@gmail.com>
2024-07-27 14:13:47 -0300

    feat: Adicionando endpoint - findAll

--- appquarkus/src/main/java/tech/buildrun/controller/UserController.java
+++ appquarkus/src/main/java/tech/buildrun/controller/UserController.java
@@ -7,6 +7,8 @@ import jakarta.ws.rs.core.Response;
 import tech.buildrun.entity.UserEntity;
 import tech.buildrun.service.UserService;
 
+import java.util.UUID;
+
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -16,6 +18,14 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GET
+    public Response findAll(@QueryParam("page") @DefaultValue("0") Integer page,
+                            @QueryParam("pageSize") @DefaultValue("10") Integer pageSize) {
+        var users = userService.findAll(page, pageSize);
+
+        return Response.ok(users).build();
     }
 
     @POST
--- appquarkus/src/main/java/tech/buildrun/service/UserService.java
+++ appquarkus/src/main/java/tech/buildrun/service/UserService.java
@@ -3,11 +3,19 @@ package tech.buildrun.service;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.buildrun.entity.UserEntity;
 
+import java.util.List;
+
 @ApplicationScoped
 public class UserService {
 
     public UserEntity createUser(UserEntity userEntity) {
         UserEntity.persist(userEntity);
         return userEntity;
+    }
+
+    public List<UserEntity> findAll(Integer page, Integer pageSize) {
+        return UserEntity.findAll()
+                .page(page, pageSize)
+                .list();
     }
 }
